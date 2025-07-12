ad3a14ad0f5ab6c32d75a6870c30271c16cd67e1
Mark Bueno <mark.bueno17@icloud.com>
2025-07-12 19:10:44 +0200

    Toolbar + PrimeFlex Implementation

--- WebUI/src/app/core/components/main-layout/main-layout.ts
+++ WebUI/src/app/core/components/main-layout/main-layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Toolbar } from 'primeng/toolbar';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.sass',
 })
