0da427c16eed7388598aad42d42888b113ca3d35
Mark Bueno <mark.bueno17@icloud.com>
2025-07-12 22:43:34 +0200

    WIP: Implement landing page

--- WebUI/src/app/features/landing-page/components/landing-page.ts
+++ WebUI/src/app/features/landing-page/components/landing-page.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Button } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [Button],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.sass',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LandingPage {}
+export default class LandingPage {
+  private readonly router = inject(Router);
+
+  public onNavigateToStrategyTool(): void {
+    this.router.navigate(['strategy-tool']);
+  }
+}
