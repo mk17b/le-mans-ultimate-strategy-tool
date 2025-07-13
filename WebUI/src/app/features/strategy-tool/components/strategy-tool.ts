166a9f7b9e3212e69b0b40e59423a369afbb9edd
Mark Bueno <mark.bueno17@icloud.com>
2025-07-13 11:32:11 +0200

    Select Basic Implementation

--- WebUI/src/app/features/strategy-tool/components/strategy-tool.ts
+++ WebUI/src/app/features/strategy-tool/components/strategy-tool.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { Select } from 'primeng/select';
+import { SelectItem } from '../../../shared/models/select-item';
 
 @Component({
   selector: 'app-strategy-tool',
-  imports: [],
+  imports: [Select],
   templateUrl: './strategy-tool.html',
   styleUrl: './strategy-tool.sass',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class StrategyTool {}
+export default class StrategyTool {
+  public cars = signal<SelectItem[]>([]);
+  public tracks = signal<SelectItem[]>([]);
+
+  public ngOnInit(): void {
+    this.cars.set([{ label: 'Ferrari', value: 'Ferrari' }]);
+    this.tracks.set([{ label: 'Le Mans', value: 'Le Mans' }]);
+  }
+}
